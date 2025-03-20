b2b9f97b5753559d70b5a1c30284d267d6db50f9
RoseSecurity <michaeljrosenfeld22@gmail.com>
2025-03-20 13:17:24 -0400

    test tfsec

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -5,3 +5,14 @@ locals {
 terraform {
   required_version = ">= 1.0"
 }
+
+resource "aws_s3_bucket" "insecure_bucket" {
+  bucket = "my-insecure-bucket"
+
+  acl = "public-read" # tfsec will flag this as a security risk
+
+  tags = {
+    Name        = "InsecureBucket"
+    Environment = "Dev"
+  }
+}
