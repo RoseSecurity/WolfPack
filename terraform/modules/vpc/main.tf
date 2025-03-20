e76d23080604bdf38c6db962793e212508f229cf
RoseSecurity <michaeljrosenfeld22@gmail.com>
2025-03-19 21:42:56 -0400

    test tflint

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -1,50 +1,23 @@
-locals {
-  create_vpc = var.create_vpc && var.rosesecurity_rocks
-  tags       = merge(var.name, var.vpc_tags)
-  vpc_name   = "${var.name}-${module.vpc.name}"
-  sg_name    = "${var.name}-${module.sg.name}"
+provider "aws" {
+  region = "us-east-1"
 }
 
-# Create VPC
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "5.9.0"
-
-  create_vpc = local.create_vpc ? 1 : 0
-  name       = local.vpc_name
-  cidr       = var.cidr
-
-  create_igw         = true
-  igw_tags           = local.tags
-  single_nat_gateway = true
-  public_subnets     = var.public_subnets
-  private_subnets    = var.private_subnets
-
-  enable_flow_log                      = true
-  create_flow_log_cloudwatch_log_group = true
-  create_flow_log_cloudwatch_iam_role  = true
-  flow_log_max_aggregation_interval    = 60
-
-  vpc_tags = local.tags
-  tags     = var.tags
+resource "aws_instance" "example" {
+  ami           = "ami-12345678" # Invalid: TFLint may warn about hardcoded AMI IDs
+  instance_type = "t2.micro"     # Warning: May suggest a more recent instance type
+  tags = {
+    Name = "ExampleInstance"
+  }
 }
 
-# Allow HTTP, HTTPS, SSH traffic
-module "sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "5.1.2"
+resource "aws_security_group" "example" {
+  name        = "example_sg"
+  description = "Example security group"
 
-  name          = local.sg_name
-  description   = "Security group for ports open within VPC"
-  vpc_id        = module.vpc.vpc_id
-  ingress_rules = ["ssh", "http-80-tcp", "https-443-tcp"]
-
-  # This can be scoped to target network
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-}
-
-
-locals {
-  list  = [{ a = "b" }, { a = "c" }]
-  value = list.*.a
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Critical: Open SSH to the world
+  }
 }
--- terraform/modules/vpc/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vpc_id" {
-  description = "The ID of the VPC"
-  value       = try(module.vpc.vpc_id, null)
-}
-
-output "vpc_cidr_block" {
-  description = "The CIDR block of the VPC"
-  value       = try(module.vpc.vpc_cidr_block, null)
-}
